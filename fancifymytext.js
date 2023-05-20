87dfdc181457e48d62ce56717f6594585e558854
Ardeniel Ulangkaya <72841623+Hollowhine@users.noreply.github.com>
2023-05-19 19:05:58 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,26 @@
+alert("Hello, world!");
+
+function Bigger(){
+    document.getElementById("textbox").style.fontSize = "24pt";
+}
+
+function Fancify(){
+    document.getElementById("textbox").style.fontWeight = "Bold";
+    document.getElementById("textbox").style.textDecoration="Underline";
+    document.getElementById("textbox").style.color = "Blue";
+}
+
+function Boring(){
+    document.getElementById("textbox").style.fontWeight = "Normal";
+    document.getElementById("textbox").style.color = "Black";
+}
+function Moo(){
+ 
+    const text=document.getElementById("textbox").value;
+
+    const CapitalText = text.toUpperCase();
+    
+    const splitText=CapitalText.split(".");
+    
+    document.getElementById("textbox").value=splitText.join("_Moo");
+}
